1293fa89869622f5c0bbd66e52d65f3c011b52be
Valera <fazlyvalera@gmail.com>
2012-08-23 01:29:24 +0300

    - update style.css

--- js/script.js
+++ js/script.js
@@ -17,8 +17,8 @@ $.parseYouTubeData = function(result){
 		var date = new Date(item.uploaded);
 		//var item_date = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
 		var item_date = date.toLocaleDateString();
-		var thumb = '<span class="thumbnail_wrapper"><img src="'+ item.thumbnail.sqDefault +'" alt="'+ item.title +'" title="'+ item.title +'" /><time>'+ $.formatTime(item.duration) +'</time></span>';
-		var item_link = '<a href="#movie_'+ item.id +'" title="'+ item.title +'">'+ thumb +'<strong>'+ item.title +'</strong><span class="author">'+ item.uploader +'</span><span class="viewCount">'+ item.viewCount +'</span><span class="date">'+ item_date +'</span></a>'
+		var thumb = '<span class="thumbnail_wrapper"><img src="'+ item.thumbnail.hqDefault +'" alt="'+ item.title +'" title="'+ item.title +'" /><time>'+ $.formatTime(item.duration) +'</time></span>';
+		var item_link = '<a href="#movie_'+ item.id +'" class="clearfix" title="'+ item.title +'">'+ thumb +'<span class="wrap"><strong>'+ item.title +'</strong><span class="author">'+ item.uploader +'</span><span class="date">'+ item_date +'</span><span class="viewCount">'+ item.viewCount +' просмотров</span></span></a>'
 		list_elements += '<li>'+ item_link +'</li>';
 	}
 	var list = '<ul>'+ list_elements +'</ul>';
@@ -27,7 +27,7 @@ $.parseYouTubeData = function(result){
 $.changeVideo = function(id){
 	$.get('http://gdata.youtube.com/feeds/api/videos/'+ id +'?v=2&alt=jsonc', {}, function(result){
 		var video = result.data;
-		var code = '<div id=\"movie_' + video.id + '\" class=\"activemovie\"><h1>'+ video.title +'</h1><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + video.content[5] + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + video.content[5] + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
+		var code = '<div id=\"movie_' + video.id + '\" class=\"activemovie\"><h3>'+ video.title +'</h3><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + video.content[5] + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + video.content[5] + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
 		$('#video').html(code);
 		$('html, body').animate({
 			scrollTop: $('#video').offset().top
@@ -37,7 +37,6 @@ $.changeVideo = function(id){
 	
 }
 $(document).ready(function() {
-	
 	$.get('https://gdata.youtube.com/feeds/api/users/regruvideo/uploads?v=2&alt=jsonc',{},function(data){
 		var list = $.parseYouTubeData(data);
 		$('#video_list').html(list);
