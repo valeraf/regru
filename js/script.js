f5871819532b8c02d7c5a63929aa37fe72425b5a
Valera <fazlyvalera@gmail.com>
2012-08-23 18:02:40 +0300

    fix parser

--- js/script.js
+++ js/script.js
@@ -6,44 +6,62 @@ $.formatTime = function(seconds){
 	minutes = minutes - (hours*60);
 	return hours + ':' + minutes + ':' + seconds;
 }
-$.parseYouTubeData = function(result){
-	var items = result.data.items;
-	var list_elements = '';
-	for (var i in items){
-		var item = items[i];
-		if (i == 0){
-			$.changeVideo(item.id);
-		}
-		var date = new Date(item.uploaded);
-		//var item_date = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
-		var item_date = date.toLocaleDateString();
-		var thumb = '<span class="thumbnail_wrapper"><img src="'+ item.thumbnail.hqDefault +'" alt="'+ item.title +'" title="'+ item.title +'" /><time>'+ $.formatTime(item.duration) +'</time></span>';
-		var item_link = '<a href="#movie_'+ item.id +'" class="clearfix" title="'+ item.title +'">'+ thumb +'<span class="wrap"><strong>'+ item.title +'</strong><span class="author">'+ item.uploader +'</span><span class="date">'+ item_date +'</span><span class="viewCount">'+ item.viewCount +' просмотров</span></span></a>'
-		list_elements += '<li>'+ item_link +'</li>';
-	}
-	var list = '<ul>'+ list_elements +'</ul>';
-	return list;
-}
-$.changeVideo = function(id){
-	$.get('http://gdata.youtube.com/feeds/api/videos/'+ id +'?v=2&alt=jsonc', {}, function(result){
-		var video = result.data;
-		var code = '<div id=\"movie_' + video.id + '\" class=\"activemovie\"><h3>'+ video.title +'</h3><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + video.content[5] + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + video.content[5] + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
+
+$.changeVideo = function(url, id, title){
+	var code = '<div id=\"movie_' + id + '\" class=\"activemovie\"><h3>'+ title +'</h3><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + url + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + url + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
 		$('#video').html(code);
 		$('html, body').animate({
 			scrollTop: $('#video').offset().top
 		}, 'slow')
-		//document.location = document.location
-	}, 'json');
 	
 }
-$(document).ready(function() {
-	$.get('https://gdata.youtube.com/feeds/api/users/regruvideo/uploads?v=2&alt=jsonc',{},function(data){
-		var list = $.parseYouTubeData(data);
-		$('#video_list').html(list);
-	},'json');
-});
 
-$(window).bind('hashchange', function(){
-	var id = window.location.hash.split('#movie_')[1];
-	$.changeVideo(id);
-})
+function showVideos(data) {
+  var feed = data.feed;
+  var entries = feed.entry || [];
+  var list_elements = '';
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i];
+	var id = entry.id.$t;
+	var playerUrl = entries[i].media$group.media$content[0].url;
+	var id_clear = id.split('http://gdata.youtube.com/feeds/videos/')[1];
+    var title = entry.title.$t;
+	var date = new Date(entry.published.$t);
+	var item_date = date.toLocaleDateString();
+	
+	var code = '<div id="movie_' + id_clear + '" class="activemovie"><h3>'+ entries[i].title.$t +'</h3><object width="745" height="448"><param name="wmode" value="opaque" /><param name="movie" value=' + playerUrl + ' /><param name="allowFullScreen" value="true" /><param name="allowscriptaccess" value="always" /><embed src=' + playerUrl + ' type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="745" height="448" wmode="opaque"></embed></object></div>';
+	
+	var uploader = entry.author.name;
+	var time = entries[i].media$group.yt$duration.seconds;
+    var thumbnailUrl = entries[i].media$group.media$thumbnail[0].url;
+	var thumb = '<span class="thumbnail_wrapper"><img src="'+ thumbnailUrl +'" alt="'+ title +'" title="'+ title +'" /><time>'+ $.formatTime(time) +'</time></span>';	
+	var stat = entry.yt$statistics.viewCount;
+	var uploader = entry.author[0].name.$t;
+	var item_link = '<a href="#movie_'+ id_clear +'" class="clearfix" title="'+ title +'">'+ thumb +'<span class="wrap"><strong>'+ title +'</strong><span class="author">'+ uploader +'</span><span class="date">'+ item_date +'</span><span class="viewCount">'+ stat +' просмотров</span></span></a>'
+	list_elements += '<li>'+ item_link +'</li>';
+  }
+  	var list = '<ul>'+ list_elements +'</ul>';
+  $('#video_list').html(list);
+  
+  if (entries.length > 0) {
+	var code = '<div id=\"movie_' + entries[0].id.$t.split('http://gdata.youtube.com/feeds/videos/')[1] + '\" class=\"activemovie\"><h3>'+ entries[0].title.$t +'</h3><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + entries[0].media$group.media$content[0].url + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + entries[0].media$group.media$content[0].url + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
+		$('#video').html(code);
+  }
+  
+  $(window).bind('hashchange', function(){
+	var id = window.location.hash.split('#movie_')[1];	
+  })
+  
+}
+
+$(document).ready(function() {	
+$('#video_list li a').each(function(){
+		$(this).click(function(){
+			
+			var title = $(this).find('strong').text();
+			var movie_id = $(this).attr('href').split('#movie_')[1];
+			var url = 'http://www.youtube.com/v/'+movie_id;
+			$.changeVideo(url, movie_id, title);
+		})
+	})
+});
