b6201ff39738a64a9c33e6fc3f564152037b392b
Valera <fazlyvalera@gmail.com>
2012-08-31 14:10:13 +0300

    - fied (просмотров/ы)

--- js/script.js
+++ js/script.js
@@ -29,7 +29,8 @@ function showVideos(data) {
 	entries = feed.entry || [];
 	list_elements = '';
     for (i = 0; i < entries.length; i++) {
-		var entry, id, playerUrl, id_clear, title, uploader, time, thumbnailUrl, thumb, stat, uploader, date, item_date, item_link, list;
+		var entry, id, playerUrl, id_clear, title, uploader, time, thumbnailUrl, thumb, stat, uploader, date, item_date, item_link, list, text, last_digit;
+		text = '';
     	entry = entries[i];
 		id = entry.id.$t;
 		playerUrl = entries[i].media$group.media$content[0].url;
@@ -41,10 +42,19 @@ function showVideos(data) {
     	thumbnailUrl = entries[i].media$group.media$thumbnail[0].url;
 		thumb = '<span class="thumbnail_wrapper"><img src="'+ thumbnailUrl +'" alt="'+ title +'" title="'+ title +'" /><time>'+ $.formatTime(time) +'</time></span>';	
 		stat = entry.yt$statistics.viewCount;
+		last_digit = stat.substring( stat.length-2 , stat.length );
+		if (last_digit >= 11 && last_digit <= 20){
+			text = 'просмотров'
+		}else{
+			last_digit = stat.substring( stat.length-1 , stat.length );
+			if(last_digit >= 5 || last_digit == 0){text = 'просмотров'}
+			if(last_digit == 1){text = 'просмотр'}
+			if(last_digit >= 2 && last_digit < 5){text = 'просмотра'}
+		}		
 		uploader = entry.author[0].name.$t;
 		moment.lang('ru');
 		item_date = moment(entry.published.$t).format('LL');
-		item_link = '<a href="#movie_'+ id_clear +'" class="clearfix" title="'+ title +'">'+ thumb +'<span class="wrap"><strong>'+ title +'</strong><span class="author">'+ uploader +'</span><span class="date">'+ item_date +'</span><span class="viewCount">'+ stat +' просмотров</span></span></a>'
+		item_link = '<a href="#movie_'+ id_clear +'" class="clearfix" title="'+ title +'">'+ thumb +'<span class="wrap"><strong>'+ title +'</strong><span class="author">'+ uploader +'</span><span class="date">'+ item_date +'</span><span class="viewCount">'+ stat +' '+ text  +'</span></span></a>'
 		list_elements += '<li>'+ item_link +'</li>';
 	}
   	list = '<ul>'+ list_elements +'</ul>';
