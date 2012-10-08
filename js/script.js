f35cf71aad83840a9b47a83d061e4b81f1d73298
Valera <fazlyvalera@gmail.com>
2012-10-08 23:09:35 +0300

    *fix
    1) убрать из $ то, чему там не место
    2) избавиться от дублирования строки div id=\"movie_...
    3) отступ 4 пробела, вокруг управляющих конструкций пробелы, т.е. "if
    ( ", "} else {" , "for (" итп. подробнее о наших стандартах:

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$.videos = new Array();
+var videos = new Array();
 
-$.formatTime = function(seconds){
+function formatTime(seconds){
 	var minutes, seconds, hours;
 	seconds = parseInt(seconds);
 	minutes = Math.floor(seconds/60);
@@ -8,16 +8,18 @@ $.formatTime = function(seconds){
 	hours = Math.floor(minutes/60);
 	minutes = minutes - (hours*60);
 	if(minutes < 10){
-		minutes = '0'+minutes;
+	    minutes = '0'+minutes;
 	}
 	if(seconds < 10){
 		seconds = '0'+seconds;
 	}
 	return hours + ':' + minutes + ':' + seconds;
 }
-
-$.changeVideo = function(url, id, title){
-	var code = '<div id=\"movie_' + id + '\" class=\"activemovie\"><h3>'+ title +'</h3><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + url + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + url + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
+function getYouTubeVideo(url, id, title){
+	return '<div id=\"movie_' + id + '\" class=\"activemovie\"><h3>'+ title +'</h3><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + url + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + url + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
+}
+function changeVideo(url, id, title){
+	var code = getYouTubeVideo(url, id, title);
 	$('#video').html(code);
 	$('html, body').animate({
 		scrollTop: $('#video').offset().top
@@ -35,17 +37,18 @@ function showVideos(data) {
 		id = entry.id.$t;
 		playerUrl = entries[i].media$group.media$content[0].url;
 		id_clear = id.split('http://gdata.youtube.com/feeds/videos/')[1];
-		$.videos['#movie_'+id_clear] = entry;
+		videos['#movie_'+id_clear] = entry;
     	title = entry.title.$t;
 		uploader = entry.author.name;
 		time = entries[i].media$group.yt$duration.seconds;
     	thumbnailUrl = entries[i].media$group.media$thumbnail[0].url;
-		thumb = '<span class="thumbnail_wrapper"><img src="'+ thumbnailUrl +'" alt="'+ title +'" title="'+ title +'" /><time>'+ $.formatTime(time) +'</time></span>';	
+		thumb = '<span class="thumbnail_wrapper"><img src="'+ thumbnailUrl +'" alt="'+ title +'" title="'+ title +'" /><time>'+ formatTime(time) +'</time></span>';	
 		stat = entry.yt$statistics.viewCount;
 		last_digit = stat.substring( stat.length-2 , stat.length );
 		if (last_digit >= 11 && last_digit <= 20){
 			text = 'просмотров'
-		}else{
+		}
+		else{
 			last_digit = stat.substring( stat.length-1 , stat.length );
 			if(last_digit >= 5 || last_digit == 0){text = 'просмотров'}
 			if(last_digit == 1){text = 'просмотр'}
@@ -62,10 +65,10 @@ function showVideos(data) {
   
 	if (entries.length > 0) {
 		var video_entry = entries[0];
-		if(window.location.hash != '' && $.videos[window.location.hash] != undefined){
-			video_entry = $.videos[window.location.hash];
+		if(window.location.hash != '' && videos[window.location.hash] != undefined){
+			video_entry = videos[window.location.hash];
 		}
-		code = '<div id=\"movie_' + video_entry.id.$t.split('http://gdata.youtube.com/feeds/videos/')[1] + '\" class=\"activemovie\"><h3>'+ video_entry.title.$t +'</h3><object width=\"745\" height=\"448\"><param name=\"wmode\" value=\"opaque\" /><param name=\"movie\" value=' + video_entry.media$group.media$content[0].url + ' /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowscriptaccess\" value=\"always\" /><embed src=' + video_entry.media$group.media$content[0].url + ' type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"745\" height=\"448\" wmode=\"opaque\"></embed></object></div>';
+		code = getYouTubeVideo(video_entry.media$group.media$content[0].url, video_entry.id.$t.split('http://gdata.youtube.com/feeds/videos/')[1], video_entry.title.$t);
 		$('#video').html(code);
   	}
   
@@ -78,6 +81,6 @@ $(document).ready(function() {
 		title = $(this).find('strong').text();
 		movie_id = $(this).attr('href').split('#movie_')[1];
 		url = 'http://www.youtube.com/v/'+movie_id;
-		$.changeVideo(url, movie_id, title);
+		changeVideo(url, movie_id, title);
 	});
 });
